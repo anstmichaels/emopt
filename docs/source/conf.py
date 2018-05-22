dc7ae90267c593105429b0799a241c2640e200c2
Andy Michaels <anstmichaels@gmail.com>
2018-05-21 18:35:14 -0700

    some more documentation tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../emopt/'))
-print os.getcwd()
+
+import glob
+print glob.glob('../../emopt/*')
 
 
 # -- General configuration ------------------------------------------------
