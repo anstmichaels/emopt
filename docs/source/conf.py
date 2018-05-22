c506a3fa1422996205676f7c0af4bdf2e5e4e3c5
Andy Michaels <anstmichaels@gmail.com>
2018-05-21 18:29:46 -0700

    some more documentation tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,6 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./emopt/'))
-sys.path.insert(0, os.path.abspath('../emopt/'))
 sys.path.insert(0, os.path.abspath('../../emopt/'))
 print os.getcwd()
 
