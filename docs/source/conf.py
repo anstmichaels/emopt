faa8b60a9decc4ee21e29f4b9fd74a5fc3b3051a
Andy Michaels <anstmichaels@gmail.com>
2018-05-21 18:23:29 -0700

    working on documentation some more

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages',
-    'sphinxcontrib.fulltoc']
+    'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
