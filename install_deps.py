69b1f7789b94a5de013d6dce38c361f7fd39d5df
Andy Michaels <anstmichaels@gmail.com>
2021-01-14 21:41:58 -0800

    Reverting to previous setup.py and install_deps.py process. PETSc and SLEPc installation make extremely difficult to install everything automatically through pip. Using install_deps.py to install petsc and slepc to a custom (known) prefix greatly eases the process

---
+++ install_deps.py
@@ -0,0 +1,218 @@
+"""A script which downloads, compiles, and installs the dependencies needed to
+install and run EMopt.
+
+EMopt depends on 2 core open source software packages:
+
+    3. PETSc -- solving large Ax=b problems, parallel computing
+    4. SLEPc -- solving large eigenvalue problems in parallel
+
+To run this script, simply call:
+
+    $ python3 install.py
+
+By default, this will create the directory ~/.emopt and install all of the
+libraries there. If you want to install these files elsewhere, you can use the
+prefix flag:
+
+    $ python3 install.py --prefix=/custom/install/path
+
+For example, for a system-wide install, we might use:
+
+    $ python3 install.py --prefix=/opt/local
+
+where /opt/local is an existing directory.
+
+If this script fails for any reason, read through the output and check the
+install.log file which should be created. This should give you some indication
+of what went wrong. In most cases, the issue will be related to not having the
+appropriate prerequisite software packages installed.
+"""
+
+import os, sys, shutil, glob, requests, pathlib
+from subprocess import call
+from argparse import ArgumentParser
+
+# EMopt parameters
+emopt_dep_file = ".emopt_deps"
+
+# Package Parameters
+PETSC_VERSION = "3.13.4"
+SLEPC_VERSION = "3.13.4"
+
+
+class Logger(object):
+    """Setup log file."""
+
+    def __init__(self, log_fname):
+        self.terminal = sys.stdout
+
+        # clean up old log
+        if(os.path.isfile(log_fname)):
+            os.remove(log_fname)
+
+        self.log = open(log_fname, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+def print_message(s):
+    """Define custom print message which adds color."""
+    print(''.join(['\033[92m', s, '\033[0m']))
+
+def install_begin(build_dir):
+    """Prepare for building dependencies.
+    This primarily involves moving into the build directory.
+    """
+    os.chdir(build_dir)
+
+def install_end(start_dir, build_dir):
+    """Cleanup post installation, i.e. delete build dir."""
+    os.chdir(start_dir)
+    if(os.path.exists(build_dir)):
+        shutil.rmtree(build_dir)
+
+def write_deps_file(home_dir, include_dir, install_dir):
+    """Generate the dependency file.
+
+    The dependency file, which is stored at ./.emopt_deps, contains the paths
+    of the installed dependencies. This is loaded by the setup.py script used
+    to install EMopt.
+    """
+    dep_fname = home_dir + '/' + emopt_dep_file
+    with open(dep_fname, 'w') as fdep:
+        fdep.write('PETSC_DIR=' + install_dir + '\n')
+        fdep.write('PETSC_ARCH=\n')
+        fdep.write('SLEPC_DIR=' + install_dir + '\n')
+
+def install_petsc(install_dir):
+    """Compile and install PETSc."""
+    # Clean up environment variables. If these are set the PETSc compilation will
+    # fail
+    if('PETSC_DIR'  in os.environ): del os.environ['PETSC_DIR']
+    if('PETSC_ARCH' in os.environ): del os.environ['PETSC_ARCH']
+
+    # get PETSc
+    print_message('Downloading PETSc...')
+    petsc_url = "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-" + PETSC_VERSION + \
+                ".tar.gz"
+    petsc_fname = "petsc-" + PETSC_VERSION + ".tar.gz"
+    r = requests.get(petsc_url, allow_redirects=True)
+    with open(petsc_fname, 'wb') as fsave:
+        fsave.write(r.content)
+
+    # unzip package
+    call(['tar', 'xvzf', petsc_fname])
+
+    petsc_folder = "petsc-" + PETSC_VERSION
+    os.chdir(petsc_folder)
+
+    # compile
+    print_message('Compiling PETSc...')
+    call(["./configure", "--with-scalar-type=complex", "--with-mpi=1",
+          "--COPTFLAGS='-O3'", "--FOPTFLAGS='-O3'", "--CXXOPTFLAGS='-O3'",  
+          "--with-debugging=0", "--prefix="+install_dir, "--download-scalapack", 
+          "--download-mumps", "--download-openblas"])
+    call(['make', 'all'])
+    call(['make', 'check'])
+
+    print_message('Installing PETSc...')
+    call(['make', 'install'])
+    os.environ['PETSC_DIR'] = install_dir
+
+    # cleanup
+    print_message('Cleaning up working directory...')
+    os.chdir('../')
+    shutil.rmtree(petsc_folder)
+
+def install_slepc(install_dir):
+    """Compile and install SLEPc."""
+    # SLEPC_DIR environment var cant be set
+    if('SLEPC_DIR' in os.environ): del os.environ['SLEPC_DIR']
+
+    # get the SLEPc source
+    print_message('Downloading SLEPc source...')
+    slepc_url = "http://slepc.upv.es/download/distrib/slepc-" + SLEPC_VERSION + \
+                ".tar.gz"
+    slepc_fname = "slepc-" + SLEPC_VERSION + ".tar.gz"
+    r = requests.get(slepc_url, allow_redirects=True)
+    with open(slepc_fname, 'wb') as fsave:
+        fsave.write(r.content)
+
+    # unzip package
+    call(['tar', 'xvzf', slepc_fname])
+
+    # compile and install
+    print_message('Compiling SLEPc...')
+    slepc_folder = "slepc-" + SLEPC_VERSION
+    os.chdir(slepc_folder)
+    call(['./configure', '--prefix='+install_dir])
+    call(['make', 'all'])
+    call(['make', 'install'])
+    call(['make', 'test'])
+
+    # cleanup
+    os.chdir('../')
+    shutil.rmtree(slepc_folder)
+    os.remove(slepc_fname)
+
+def install_deps(prefix=None):
+    # setup logging
+    sys.stdout = Logger("install.log")
+
+    # setup install directory
+    home_dir = os.path.expanduser('~')
+    if(prefix == None):
+        install_dir = home_dir + '/.emopt/'
+    else:
+        install_dir = args.prefix
+
+    if(not os.path.exists(install_dir)):
+        os.makedirs(install_dir)
+
+    # setup installation subdirs
+    include_dir = install_dir + 'include/'
+    lib_dir = install_dir + 'lib/'
+
+    if(not os.path.exists(include_dir)):
+        os.makedirs(include_dir)
+
+    if(not os.path.exists(lib_dir)):
+        os.makedirs(lib_dir)
+
+    # setup working directory
+    current_dir = os.getcwd()
+    build_dir = './build/'
+    if(not os.path.exists(build_dir)):
+        os.makedirs(build_dir)
+
+    # save file to user's home directory which will tell emopt where to look for the
+    # dependencies
+
+
+    # install dependencies
+    install_begin(build_dir)
+    try:
+        install_petsc(install_dir)
+        install_slepc(install_dir)
+        install_end(current_dir, build_dir)
+        write_deps_file(home_dir, include_dir, install_dir)
+    except Exception as e:
+        print(e)
+        install_end(current_dir, build_dir)
+
+    print_message('Finished installing EMOpt dependencies!')
+
+if __name__ == '__main__':
+    # Do Argument parsing
+    parser = ArgumentParser()
+    parser.add_argument("--emopt-prefix=", metavar='filepath', type=str, dest='emopt_prefix', 
+                        help='Set the installation directory for EMopt dependencies')
+
+    args = parser.parse_args()
+
+    install_deps(args.emopt_prefix)
+
--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools.command.install import install as SetuptoolsInstall
 import subprocess, os, sys
 
 class MakeInstall(SetuptoolsInstall):
+    # Add a commandline argument for emopt prefix
+    user_options = SetuptoolsInstall.user_options + [('emopt-prefix=', None, None)]
 
     def initialize_options(self):
         SetuptoolsInstall.initialize_options(self)
@@ -12,21 +14,53 @@ class MakeInstall(SetuptoolsInstall):
         SetuptoolsInstall.finalize_options(self)
 
     def run(self):
-        if('PETSC_DIR' not in os.environ \
-            or 'PETSC_ARCH' not in os.environ \
-            or os.environ['PETSC_DIR'] == ''):
-            print('PETSc installation not found. It will now be compiled and installed with '
-                  'the petsc4py package. Note: this can take up to 30 minutes or more.')
+        # EMopt has a few import dependencies that need to be installed first. Some linux
+        # distributions provide these packages in their package manager (e.g. Ubunutu,
+        # and archlinux). If these packages are not provided, EMopt includes an install_deps
+        # script which can take care of the installation.
+        base_dir = os.path.dirname(os.path.realpath(__file__))
+        deps_file = base_dir + '/.emopt_deps'
+        if(os.path.exists(deps_file)):
+            with open(deps_file, 'r') as fdeps:
+                for line in fdeps:
+                    toks = line.rstrip('\r\n').split('=')
+                    os.environ[toks[0]] = toks[1]
+        else:
+            if('PETSC_DIR' not in os.environ \
+                or 'PETSC_ARCH' not in os.environ \
+                or os.environ['PETSC_DIR'] == ''):
+                try:
+                    import petsc4py, slepc4py
+                except ImportError:
+                    print('petsc4py is not currently installed, but is required by EMopt.')
+                    print('If the petsc-complex is available through your package manager ' \
+                          'then it is recommended that you install the following packages:')
+                    print('\t1. petsc-complex')
+                    print('\t2. slepc-complex')
+                    print('\t3. petsc4py-complex')
+                    print('\t4. slepc4py-complex')
+                    print('And then re-install EMopt.')
+                    print('')
+                    print('Alternatively, PETSc and SLEPc may be downloaded and compiled ' \
+                          'for use with EMopt automatically now.')
+                    inp = input('Proceed with installation of PETSc and SLEPc? [y/n] ')
 
-            os.environ['PETSC_CONFIGURE_OPTIONS'] = "--with-scalar-type=complex " \
-                                                    "--with-mpi=1 " \
-                                                    "--COPTFLAGS='-O3' " \
-                                                    "--FOPTFLAGS='-O3' " \
-                                                    "--CXXOPTFLAGS='-O3' " \
-                                                    "--with-debugging=0 " \
-                                                    "--download-scalapack " \
-                                                    "--download-mumps " \
-                                                    "--download-openblas"
+                    ask_input = True
+                    get_deps = False
+                    while(ask_input):
+                        if(inp.upper() == 'Y'):
+                            get_deps = True
+                            ask_input = False
+                        elif(inp.upper() == 'N'):
+                            sys.exit(0)
+                        else:
+                            print("Please enter 'y' to proceed with the installation " \
+                                        "or 'n' to abort. ")
+                            inp = input('Proceed with installation of PETSc and SLEPc? [y/n] ')
+
+                        if(get_deps):
+                            import install_deps
+                            install_deps.install_deps(self.emopt_prefix)
 
         subprocess.call('make')
         SetuptoolsInstall.do_egg_install(self)
@@ -51,5 +85,5 @@ setup(name='emopt',
       packages=find_packages(),
       package_data={'emopt':['*.so', '*.csv', 'data/*', 'solvers/*.so']},
       cmdclass={'install':MakeInstall},
-      install_requires=['numpy', 'scipy', 'matplotlib', 'mpi4py', 'petsc', 'petsc4py', 'slepc', 'slepc4py', 'h5py'],
+      install_requires=['numpy', 'scipy', 'mpi4py', 'petsc4py', 'slepc4py'],
       zip_safe=False)
