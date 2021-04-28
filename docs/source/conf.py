6d42b28bd4404a989f35cd3dbb0993d6c9f8a27e
anstmichaels <anstmichaels@gmail.com>
2021-04-28 09:30:21 -0700

    Update conf.py
    fixed typos

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EMopt'
-copyright = u'2019, University of California, Berkeley'
+copyright = u'2019, Andrew Michaels'
 author = u'Andrew Michaels'
 
 # The version info for the project you're documenting, acts as replacement for
